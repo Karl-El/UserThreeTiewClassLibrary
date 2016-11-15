d2ef6e07e1114d30af02b8efa6c62871ecefccd3
KarlKrus <carl_son_cruz@live.com>
2016-11-15 09:39:13 +0800

    11.15.16.09.38.AM //UserBusiness

---
+++ BusinessLayer/BusinessUser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using DataAccessLayer;
+
+namespace BusinessLayer
+{
+    public class BusinessUser
+    {
+        DataAccessUser DA = new DataAccessUser();
+        SqlCommand Cmd = new SqlCommand();
+        public int User_Id { get; set; }
+        public string User_Name { get; set; }
+        public string User_Email { get; set; }
+        public string User_Pass { get; set; }
+        public int User_TypeID { get; set; }
+
+        public DataSet UserSelect()
+        {
+            return DA.Select_User();
+        }
+
+        public void UserInsert()
+        {
+            Cmd.Parameters.AddWithValue("@USERID", User_Id);
+            Cmd.Parameters.AddWithValue("@USERNAME", User_Name);
+            Cmd.Parameters.AddWithValue("@EMAIL", User_Email);
+            Cmd.Parameters.AddWithValue("@USERPASS", User_Pass);
+            Cmd.Parameters.AddWithValue("@USERTYPEID", User_TypeID);
+            DA.Insert_User(Cmd);
+        }
+
+        public void UserUpdate(int UserID)
+        {
+            Cmd.Parameters.AddWithValue("@USERID", User_Id);
+            Cmd.Parameters.AddWithValue("@USERNAME", User_Name);
+            Cmd.Parameters.AddWithValue("@EMAIL", User_Email);
+            Cmd.Parameters.AddWithValue("@USERPASS", User_Pass);
+            Cmd.Parameters.AddWithValue("@USERTYPEID", User_TypeID);
+            DA.Update_User(Cmd);
+        }
+
+        public void UserDelete(int UserID)
+        {
+            Cmd.Parameters.AddWithValue("@USERID", User_Id);
+            DA.Delete_User(Cmd);
+        }
+    }
+}
